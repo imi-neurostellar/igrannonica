2e683211fb1b2a451843444790cd2f6df5b0b6c6
Ognjen Cirkovic <ciraboxkg@gmail.com>
2022-02-28 23:31:04 +0100

    Dodat test projekat.Povezan front i back.

---
+++ sandbox/TestAppOgnjen/TestAppOgnjen/TestAppOgnjen/Db.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace TestAppOgnjen
+{
+    public class Db
+    {
+        private static SqliteConnection _connection = null;
+
+        public static SqliteConnection conn
+        {
+            get
+            {
+                if (_connection == null)
+                {
+                    _connection = new SqliteConnection("Data Source=DB.db");
+                }
+                return _connection;
+            }
+        }
+
+
+    }
+}
