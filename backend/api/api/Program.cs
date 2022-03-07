2e8e7bad8f7cf0c5c333ac4bf95381defbdf4ae0
Ognjen Cirkovic <ciraboxkg@gmail.com>
2022-03-07 19:56:52 +0100

    Dodat Cors.

--- backend/api/api/Program.cs
+++ backend/api/api/Program.cs
@@ -1,10 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Add Cors
+builder.Services.AddCors();
+
+// Add services to the container
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+
+//Add Cors
+app.UseCors(
+    x=>x.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    );
 
 // Configure the HTTP request pipeline.
 
