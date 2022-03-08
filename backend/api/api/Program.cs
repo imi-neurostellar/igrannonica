158c874a459b41cfacbd0238230dc5f48f481d44
DESKTOP-S0O2C44\ROG <ivan996sk@gmail.com>
2022-03-08 17:19:37 +0100

    Odradjena konekcija sa bazom. Odradjen kontroler za korisnika(get,post, put, delete).

--- backend/api/api/Program.cs
+++ backend/api/api/Program.cs
@@ -1,9 +1,28 @@
+using api.Data;
+using api.Interfaces;
+using api.Services;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add Cors
 builder.Services.AddCors();
 
 // Add services to the container
+//dodajemo dep inj
+
+builder.Services.Configure<UserStoreDatabaseSettings>(
+    builder.Configuration.GetSection(nameof(UserStoreDatabaseSettings)));
+
+builder.Services.AddSingleton<IUserStoreDatabaseSettings>(sp =>
+    sp.GetRequiredService<IOptions<UserStoreDatabaseSettings>>().Value);
+
+builder.Services.AddSingleton<IMongoClient>(s =>
+    new MongoClient(builder.Configuration.GetValue<string>("UserStoreDatabaseSettings:ConnectionString")));
+
+builder.Services.AddScoped<IUserService, UserService>();
+ 
 
 builder.Services.AddControllers();
 
---
+++ backend/api/api/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using api.Models;
+
+namespace api.Services
+{
+    public interface IUserService
+    {
+        List<User> Get();// daje sve korisnike
+        User Get(string id); //daje korisnika po id-u
+        User Create(User user); // kreira korisnika
+        void Update(string id, User user); //apdejruje korisnika po idu
+        void Delete(string id);//brise korisnika
+    }
+}
