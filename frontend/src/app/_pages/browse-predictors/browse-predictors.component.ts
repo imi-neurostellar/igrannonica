0d8e218c0d5082ffd390202d78e9fa9078ade71f
Ognjen Cirkovic <ciraboxkg@gmail.com>
2022-03-23 13:56:10 +0100

    Omoguceno da dugme otvara predictor stranicu.

--- frontend/src/app/_pages/browse-predictors/browse-predictors.component.ts
+++ frontend/src/app/_pages/browse-predictors/browse-predictors.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PredictorsService } from 'src/app/_services/predictors.service';
 import Predictor from 'src/app/_data/Predictor';
+import {Router} from '@angular/router'
 @Component({
   selector: 'app-browse-predictors',
   templateUrl: './browse-predictors.component.html',
@@ -10,7 +11,7 @@ export class BrowsePredictorsComponent implements OnInit {
 
   myPredictors? :Predictor[];
   term: string="";
-  constructor(private predictors: PredictorsService) {
+  constructor(private predictors: PredictorsService,private router:Router) {
     this.predictors.getPublicPredictors().subscribe((predictors) => {
       this.myPredictors = predictors;
     });
@@ -18,5 +19,8 @@ export class BrowsePredictorsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  openPredictor(id:string):void{
+    this.router.navigateByUrl('/predict?id='+id);
+  };
 
 }
