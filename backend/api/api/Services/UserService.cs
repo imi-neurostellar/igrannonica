28e3d25b5d4756361b464e8ca34fbc986fc0e75f
Ognjen Cirkovic <ciraboxkg@gmail.com>
2022-03-29 17:10:29 +0200

    Kada korisnik menja username dodata provera da li taj username vec postoji u bazi.Prilikom menjanja username-a updajtaju se sve ostale tabele koje sadrze stari username novim username-om korisnika.Vraca povratnu informaciju da li je operacija uspesna. Dodati mehanizmi za transakcije.(Unkomentarisati ih kada se dozvoli da baza podrzava transakcije).

--- backend/api/api/Services/UserService.cs
+++ backend/api/api/Services/UserService.cs
@@ -7,11 +7,22 @@ namespace api.Services
     public class UserService : IUserService 
     {
         private readonly IMongoCollection<User> _users;
+        private readonly IMongoClient _client;
+        private readonly IMongoCollection<Model> _models;
+        private readonly IMongoCollection<Dataset> _datasets;
+        private readonly IMongoCollection<FileModel> _fileModels;
+        private readonly IMongoCollection<Predictor> _predictors;
+
 
         public UserService(IUserStoreDatabaseSettings settings, IMongoClient mongoClient)
         {
             var database = mongoClient.GetDatabase(settings.DatabaseName);
              _users = database.GetCollection<User>(settings.CollectionName);
+            _models = database.GetCollection<Model>(settings.ModelCollectionName);
+            _datasets= database.GetCollection<Dataset>(settings.DatasetCollectionName);
+            _fileModels = database.GetCollection<FileModel>(settings.FilesCollectionName);
+            _predictors= database.GetCollection<Predictor>(settings.PredictorCollectionName);
+            _client = mongoClient;
         }
         public User Create(User user)
         {
@@ -26,10 +37,46 @@ namespace api.Services
         {
             return _users.Find(user => user.Username == username).FirstOrDefault();
         }
-        public void Update(string username, User user)
+        public bool Update(string username, User user)
         {
             //username koji postoji u bazi
-            _users.ReplaceOne(user => user.Username == username, user);
+            using (var session =  _client.StartSession())
+            {
+                
+                if(_users.Find(u => u.Username == user.Username).FirstOrDefault()!=null)
+                {
+                    return false;
+                }
+
+                //Trenutan MongoDB Server ne podrzava transakcije.Omoguciti Podrsku
+                //session.StartTransaction();
+                try
+                {
+                    _users.ReplaceOne(user => user.Username == username, user);
+                    if (username != user.Username)
+                    {
+                        var builderModel = Builders<Model>.Update;
+                        var builderDataset = Builders<Dataset>.Update;
+                        var builderFileModel = Builders<FileModel>.Update;
+                        var builderPredictor = Builders<Predictor>.Update;
+                        _models.UpdateMany(x => x.username == username, builderModel.Set(x => x.username, user.Username));
+                        _datasets.UpdateMany(x => x.username == username, builderDataset.Set(x => x.username, user.Username));
+                        _fileModels.UpdateMany(x => x.username == username, builderFileModel.Set(x => x.username, user.Username));
+                        _predictors.UpdateMany(x => x.username == username, builderPredictor.Set(x => x.username, user.Username));
+                    }
+
+                    //session.AbortTransaction();
+
+
+                    //session.CommitTransaction();
+                }
+                catch (Exception e)
+                {
+                    //session.AbortTransaction();
+                    return false;
+                }
+                return true;
+            }
         }
         public void Delete(string username)
         {
