6ffac0e8f7803e895e411328917f6c49b2520b28
Nevena Bojovic <nenabojov@gmail.com>
2022-04-04 21:46:02 +0200

    Doradjena stranica my-datasets.

--- frontend/src/app/_pages/my-datasets/my-datasets.component.ts
+++ frontend/src/app/_pages/my-datasets/my-datasets.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import { DatasetsService } from 'src/app/_services/datasets.service';
 import Dataset from 'src/app/_data/Dataset';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-my-datasets',
@@ -7,18 +11,47 @@ import Dataset from 'src/app/_data/Dataset';
   styleUrls: ['./my-datasets.component.css']
 })
 export class MyDatasetsComponent implements OnInit {
+  myDatasets: Dataset[] = [];
 
-  myDatasets?: Dataset[];
+  constructor(private datasetsS : DatasetsService) {
 
-  constructor() {
-    this.myDatasets = [
-      new Dataset('Titanik', 'Opis titanik', ['K1', 'K2', 'K3', 'Ime', 'Preziveli']),
-      new Dataset('Neki drugi set', 'opis', ['a', 'b', 'c']),
-      new Dataset('Treci set', 'opis', ['a', 'b', 'c'])
-    ];
-  }
+    
+
+     }
 
   ngOnInit(): void {
+    this.datasetsS.getMyDatasets();
+
+  }
+/*
+  editModel(): void{
+    this.modelsS.editModel().subscribe(m => {
+      this.myModel = m;
+
+    })
+  }
+*/
+
+deleteThisDataset(dataset: Dataset): void{
+  console.log("OK");
+  this.datasetsS.deleteDataset(dataset).subscribe((response) => {
+    console.log("OBRISANO JE", response);
+    //na kraju uspesnog
+    this.getAllMyDatasets();
+  }, (error) =>{
+      if (error.error == "Dataset with name = {name} deleted") {
+        alert("Greška pri brisanju dataseta!");
+      }
+    });
+
+}
+
+  getAllMyDatasets(): void{
+    this.datasetsS.getMyDatasets().subscribe(m => {
+      
+      this.myDatasets = m;
+      console.log(this.myDatasets);
+    });
   }
 
 }
