f141602c2906e72694d90e13e54d370e8b8cbd00
Ognjen Cirkovic <ciraboxkg@gmail.com>
2022-05-05 02:50:09 +0200

    Prosiren korisnik na frontu. Popravljen bug po izmeni korisnickog profila.

--- frontend/src/app/_data/User.ts
+++ frontend/src/app/_data/User.ts
@@ -6,6 +6,8 @@ export default class User {
         public password: string = '',
         public firstName: string = '',
         public lastName: string = '',
-        public photoId: string = '1' //difoltna profilna slika
+        public photoId: string = '1', //difoltna profilna slika
+        public isPermament:boolean=false,
+        public dateCreated:Date=new Date()
     ) { }
 }
