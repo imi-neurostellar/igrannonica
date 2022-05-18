b3a47535532ca7af3943196c7c78f9dd6025a5b0
Ivan Ljubisavljevic <ivan996sk@gmail.com>
2022-05-18 20:44:13 +0200

    Predictor fix

--- frontend/src/app/_data/Predictor.ts
+++ frontend/src/app/_data/Predictor.ts
@@ -3,19 +3,20 @@ import { FolderFile } from "./FolderFile";
 export default class Predictor extends FolderFile {
     constructor(
         name: string = 'Novi prediktor',
-        public description: string = '',
+        
+        public uploaderId: string = '',
         public inputs: string[] = [],
         public output: string = '',
         public isPublic: boolean = false,
         public accessibleByLink: boolean = false,
         dateCreated: Date = new Date(),
-        lastUpdated: Date = new Date(),
-        public uploaderId: string = '',
-        //public finalMetrics: Metric[] = []
         public experimentId: string = "",
         public modelId: string = "",
+        public h5FileId: string = "",
+        public metrics: Metric[] = [],
+        public finalMetrics: Metric[] = []
     ) {
-        super(name, dateCreated, lastUpdated);
+        super(name, dateCreated, dateCreated);
     }
 }
 
