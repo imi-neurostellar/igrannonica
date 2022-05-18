b3fa2aa0937fcd5ce2f3f112fe928d337f9b14f0
Nevena Bojovic <nenabojov@gmail.com>
2022-05-18 20:37:07 +0200

    Korekcija

--- frontend/src/app/_elements/form-dataset/form-dataset.component.ts
+++ frontend/src/app/_elements/form-dataset/form-dataset.component.ts
@@ -115,4 +115,156 @@ export class FormDatasetComponent {
 
   
 
+  selectNewFile() {
+    if (!this.newFile) {
+      this.createNewFile();
+    }
+    this.fileToDisplay = this.newFile;
+    this.selectedFile = this.newFile;
+    this.newFileSelected = true;
+    this.listView = false;
+    this.selectedFileChanged.emit(this.newFile);
+    this.displayFile();
+  }
+
+  selectFile(index: number) {
+    this.selectedFile = this.filteredFiles[index];
+    this.fileToDisplay = this.filteredFiles[index];
+    this.newFileSelected = false;
+    this.listView = false;
+    this.selectedFileChanged.emit(this.selectedFile);
+    this.displayFile();
+  }
+
+  createNewFile() {
+    if (this.type == FolderType.Dataset) {
+      this.newFile = new Dataset();
+    } else if (this.type == FolderType.Model) {
+      this.newFile = new Model();
+    }
+  }
+
+  ok() {
+    this.okPressed.emit();
+  }
+
+  refreshFiles(){
+    this.datasetsService.getMyDatasets().subscribe((datasets) => {
+      this.folders[TabType.MyDatasets] = datasets;
+    });
+
+    this.experimentsService.getMyExperiments().subscribe((experiments) => {
+      this.folders[TabType.MyExperiments] = experiments;
+    });
+
+    this.datasetsService.getPublicDatasets().subscribe((datasets) => {
+      this.folders[TabType.PublicDatasets] = datasets;
+    });
+
+    this.modelsService.getMyModels().subscribe((models) => {
+      this.folders[TabType.MyModels] = models;
+    });
+
+    /*this.modelsService.getMyModels().subscribe((models) => {
+      this.folders[TabType.PublicModels] = models;
+    });*/
+    this.folders[TabType.PublicModels] = [];
+
+    this.experimentsService.getMyExperiments().subscribe((experiments) => {
+      this.folders[TabType.MyExperiments] = experiments;
+    });
+
+    this.files = [];
+
+    this.filteredFiles.length = 0;
+    this.filteredFiles.push(...this.files);
+
+    this.searchTermsChanged();
+
+  }
+
+  saveNewFile() {
+    if(this.type == FolderType.Dataset)
+      this.formDataset!.uploadDataset();
+  }
+
+
+  /*calcZIndex(i: number) {
+    let zIndex = (this.files.length - i - 1)
+    if (this.selectedFileIndex == i)
+      zIndex = this.files.length + 2;
+    if (this.hoveringOverFileIndex == i)
+      zIndex = this.files.length + 3;
+    return zIndex;
+  }
+
+  newFileZIndex() {
+    return (this.files.length + 1);
+  }*/
+
+  clearSearchTerm() {
+    this.searchTerm = '';
+    this.searchTermsChanged();
+  }
+
+  filteredFiles: FolderFile[] = [];
+
+  searchTermsChanged() {
+    this.filteredFiles.length = 0;
+    this.filteredFiles.push(...this.files.filter((file) => file.name.toLowerCase().includes(this.searchTerm.toLowerCase())));
+    if (this.selectedFile) {
+      if (!this.filteredFiles.includes(this.selectedFile)) {
+        this.selectFile(-1);
+      } else {
+        this.selectedFileIndex = this.filteredFiles.indexOf(this.selectedFile);
+      }
+    }
+  }
+
+  listView: boolean = false;
+
+  toggleListView() {
+    this.listView = !this.listView;
+  }
+
+  deleteFile() {
+    console.log('delete');
+  }
+
+  folders: { [tab: number]: FolderFile[] } = {};
+
+  tabTitles: { [tab: number]: string } = {
+    [TabType.File]: 'Fajl',
+    [TabType.NewFile]: 'Novi fajl',
+    [TabType.MyDatasets]: 'Moji izvori podataka',
+    [TabType.PublicDatasets]: 'Javni izvori podataka',
+    [TabType.MyModels]: 'Moje konfiguracije neuronske mreže',
+    [TabType.PublicModels]: 'Javne konfiguracije neuronske mreže',
+    [TabType.MyExperiments]: 'Eksperimenti',
+  };
+
+  FolderType = FolderType;
+
+  TabType = TabType;
+
+  @Input() tabsToShow: TabType[] = [
+    TabType.MyDatasets,
+    TabType.PublicDatasets,
+    TabType.MyModels,
+    TabType.PublicModels,
+    TabType.MyExperiments,
+    TabType.File
+  ]
+
+  @Input() selectedTab: TabType = TabType.NewFile;
+  hoverTab: TabType = TabType.None;
+
+  selectTab(tab: TabType) {
+    this.checkListView(tab);
+    this.selectedTab = tab;
+    this.files = this.folders[tab];
+
+    this.searchTermsChanged();
+  }
+
 }
