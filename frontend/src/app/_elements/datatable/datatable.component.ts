687a93fd970f43f26b3d4b6a45dfbf5af4fbbed7
Nevena Bojovic <nenabojov@gmail.com>
2022-05-27 21:54:35 +0200

    Bug - brisanje pogresnog prediktora iz liste.

--- frontend/src/app/_elements/datatable/datatable.component.ts
+++ frontend/src/app/_elements/datatable/datatable.component.ts
@@ -24,4 +24,54 @@ export class TableData {
     public numCols = 0,
     public data?: any[][]
   ) { }
+
+  setDeleteColumnsForMissingValTreatment() {
+    if (this.experiment != undefined) {
+      this.experiment.nullValues = NullValueOptions.DeleteRows;
+      this.experiment.nullValuesReplacers = [];
+      for (let i = 0; i < this.experiment.inputColumns.length; i++) {
+        this.experiment.nullValuesReplacers.push({
+          column: this.experiment.inputColumns[i],
+          option: NullValueOptions.DeleteRows,
+          value: ""
+        });
+      }
+    }
+  }
+
+  changeInputColumns(targetMatCheckbox: MatCheckboxChange, columnName: string) {
+    if (this.experiment != undefined) {
+      if (targetMatCheckbox.checked) {
+        if (this.experiment.inputColumns.filter(x => x == columnName)[0] == undefined) {
+          this.experiment.inputColumns.push(columnName);
+        }
+      }
+      else {
+        this.experiment.inputColumns = this.experiment.inputColumns.filter(x => x != columnName);
+        //console.log("Input columns: ", this.experiment.inputColumns);
+        //TODO: da se zatamni kolona koja je unchecked
+        //this.experiment.encodings = this.experiment.encodings.filter(x => x.columnName != columnName); samo na kraju iz enkodinga skloni necekirane
+        this.experiment.nullValuesReplacers = this.experiment.nullValuesReplacers.filter(x => x.column != columnName);
+      }
+    }
+  }
+
+  resetColumnEncodings(encodingType: Encoding) {
+    if (this.experiment != undefined && this.dataset != undefined) {
+      this.experiment.encodings = [];
+      for (let i = 0; i < this.dataset?.columnInfo.length; i++) {
+        this.experiment.encodings.push(new ColumnEncoding(this.dataset?.columnInfo[i].columnName, encodingType));
+        //console.log(this.experiment.encodings);
+      }
+    }
+  }
+  openEncodingDialog() {
+    const dialogRef = this.dialog.open(EncodingDialogComponent, {
+      width: '300px'
+    });
+    dialogRef.afterClosed().subscribe(selectedEncoding => {
+      if (selectedEncoding != undefined)
+        this.resetColumnEncodings(selectedEncoding);
+    });
+  }
 }
